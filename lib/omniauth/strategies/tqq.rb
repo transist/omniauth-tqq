2ca8311a0f28473b16ba2ae4760bd5a590a91572
Scott Ballantyne <ussballantyne@gmail.com>
2011-10-31 12:19:36 +0800

    trying to get the uid

--- lib/omniauth/strategies/tqq.rb
+++ lib/omniauth/strategies/tqq.rb
@@ -67,6 +67,33 @@ module OmniAuth
       rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
         fail!(:service_unavailable, e)
       end
+      
+      def callback_phase
+        raise OmniAuth::NoSessionError.new("Session Expired") if session['oauth'].nil?
+
+        request_token = ::OAuth::RequestToken.new(consumer, session['oauth'][name.to_s].delete('request_token'), session['oauth'][name.to_s].delete('request_secret'))
+
+        opts = {}
+        if session['oauth'][name.to_s]['callback_confirmed']
+          opts[:oauth_verifier] = request['oauth_verifier']
+        else
+          opts[:oauth_callback] = callback_url
+        end
+
+        @access_token = request_token.get_access_token(opts)
+        @access_token.params[:id] = request['open_id']
+        super
+      rescue ::Timeout::Error => e
+        fail!(:timeout, e)
+      rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
+        fail!(:service_unavailable, e)
+      rescue ::OAuth::Unauthorized => e
+        fail!(:invalid_credentials, e)
+      rescue ::NoMethodError, ::MultiJson::DecodeError => e
+        fail!(:invalid_response, e)
+      rescue ::OmniAuth::NoSessionError => e
+        fail!(:session_expired, e)
+      end
 
       def raw_info
         @raw_info ||= MultiJson.decode(access_token.get('http://open.t.qq.com/api/user/info?format=json').body)
